dd7709afe42fc6e81ebf91adc285704dd71f7c88
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-09-15 21:53:52 -0400

    add drawing function

---
+++ FingerPaintApp/LineSegment.h
@@ -0,0 +1,13 @@
+//
+//  LineSegment.h
+//  FingerPaintApp
+//
+//  Created by Jaison Bhatti on 2017-09-15.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LineSegment : NSObject
+
+@end
---
+++ FingerPaintApp/LineSegment.m
@@ -0,0 +1,13 @@
+//
+//  LineSegment.m
+//  FingerPaintApp
+//
+//  Created by Jaison Bhatti on 2017-09-15.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import "LineSegment.h"
+
+@implementation LineSegment
+
+@end
